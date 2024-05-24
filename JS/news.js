9df8cc51255d760ce25802e1f3ac7d390f98008a
musthakeem98 <musthakeemahamed12122@gmail.com>
2024-05-24 10:49:58 +0530

    update the code

--- JS/news.js
+++ JS/news.js
@@ -60,5 +60,19 @@ document.addEventListener("DOMContentLoaded", function () {
       el: ".swiper-pagination-news",
       clickable: true,
     },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 70,
+      },
+    },
   });
 });
